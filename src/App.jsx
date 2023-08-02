14e14ed703c292b46aee752c6397b0aa338dbee4
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-08-02 20:49:01 +0100

    hashrouter added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  createHashRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Outlet,
+  Route,
+} from "react-router-dom";
+
 import { Home, AboutMe, Contact, BlogPage, NotFound } from "./pages";
-import Header from "./components/Header"
+import Header from "./components/Header";
 import Footer from "./components/Footer/";
 import Posts from "./components/BlogJS/Posts"; // Import the Posts component
 import Post1 from "./components/BlogJS/Post1"; // Import the Post1 component
@@ -11,31 +18,39 @@ import Post4 from "./components/BlogJS/Post4"; // Import the Post4 component
 import "./App.css";
 
 const App = () => {
+  const router = createHashRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/AboutMe" element={<AboutMe />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/BlogPage" element={<BlogPage />} />
+        <Route exact path="/" element={<Posts />} />
+        <Route path="/post1" element={<Post1 />} />
+        <Route path="/post2" element={<Post2 />} />
+        <Route path="/post3" element={<Post3 />} />
+        <Route path="/post4" element={<Post4 />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    )
+  )
+  return <RouterProvider router={router} />
+}
+
+const Root = () => {
   return (
-    <Router>
+    <>
       <div>
         <Header />
       </div>
       <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/AboutMe" element={<AboutMe />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/BlogPage" element={<BlogPage />} />
-          <Route exact path="/" element={<Posts />} />
-          <Route path="/post1" element={<Post1 />} />
-          <Route path="/post2" element={<Post2 />} />
-          <Route path="/post3" element={<Post3 />} />
-          <Route path="/post4" element={<Post4 />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Outlet />
       </div>
       <div>
         <Footer />
       </div>
-    </Router>
-  );
-};
+    </>
+  )
+}
 
-export default App;
-
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <App />
-  </React.StrictMode>,
-)
+);
