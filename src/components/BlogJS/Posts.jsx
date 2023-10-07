22396c03748c0e8f4fbefcd5d85db7c5fa24e96b
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-10-07 16:53:29 +0100

    blog post order edited

--- src/components/BlogJS/Posts.jsx
+++ src/components/BlogJS/Posts.jsx
@@ -4,15 +4,15 @@ import Post2 from "./Post2";
 import Post3 from "./Post3";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "../../assets/styles/pages.css"
+import "../../assets/styles/pages.css";
 const Posts = () => {
   return (
     <Container>
       <Row className="justify-content-between">
         <Col md={8} className="mb-4 mt-4">
-          <Post1 />
-          <Post2 />
           <Post3 />
+          <Post2 />
+          <Post1 />
         </Col>
         <Col md={2} className="mt-4 float-right">
           <Card>
@@ -20,22 +20,25 @@ const Posts = () => {
               <Card.Title>Recent Posts</Card.Title>
               <ul className="list-unstyled">
                 <li>
-                  <Link to="/Post1">
-                    A period of reflection and goal setting
-                  </Link> 15/7/2023
-                </li>
-                <br />
+                  <Link to="/Post3">
+                    My 1st week as an Associate Consultant with Version 1
+                  </Link>{" "}
+                  7/10/2023
+                </li>{" "}
                 <li>
                   <Link to="/Post2">
                     Journey to Landing my First Job in Tech
-                  </Link> 5/7/2023
+                  </Link>{" "}
+                  5/7/2023
                 </li>
                 <br />
                 <li>
-                  <Link to="/Post3">
-                    My 1st week as an Associate Consultant with Version 1
-                  </Link> 7/10/2023
+                  <Link to="/Post1">
+                    A period of reflection and goal setting
+                  </Link>{" "}
+                  15/7/2023
                 </li>
+                <br />
               </ul>
             </Card.Body>
           </Card>
